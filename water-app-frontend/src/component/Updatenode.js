d8f3d01066a3b236a38e089cb8bfeebec09d6ed2
blurry-x-face <rishabhshukla1999@gmail.com>
2020-09-03 02:18:32 +0530

    add dropdown

--- water-app-frontend/src/component/Updatenode.js
+++ water-app-frontend/src/component/Updatenode.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Axios from "axios";
 import { Redirect } from "react-router";
-import ErrorHandle from './ErrorHandle';
+import ErrorHandle from "./ErrorHandle";
 
 export default function Updatenode() {
   const [id, setId] = useState();
@@ -15,11 +15,21 @@ export default function Updatenode() {
   const [rportname, setRportname] = useState();
   const [rquantity, setRquantity] = useState();
   const [rmonth, setRmonth] = useState();
-  const [error, setError] = useState();
+  const [error, setError] = useState(null);
   const [redirect, setRedirect] = useState(false);
-  const[disable,setDisable] = useState(false);
+  const [disable, setDisable] = useState(false);
+  const [nodes, setNodes] = useState([]);
+  const [recievingNodes, setRecievingNodes] = useState([]);
+  const [distributionList, setDistributionList] = useState([]);
 
-  let getArray = a => {
+  useEffect(() => {
+    Axios.get("http://localhost:5000/nodes/nodesList").then((res) => {
+      console.log(res.data);
+      setNodes(res.data);
+    });
+  }, []);
+
+  let getArray = (a) => {
     let Arr = [];
     a.toString();
     let start = 0,
@@ -43,19 +53,26 @@ export default function Updatenode() {
     return Arr;
   };
 
-  const submit = async e => {
+  const submit = async (e) => {
     setDisable(true);
     e.preventDefault();
 
     try {
-      if(!id && !((recievingl && supplyingl) || (syear && sportname && smonth && squantity) || (ryear && rportname && rmonth && rquantity))){
+      if (
+        !id &&
+        !(
+          (recievingl && supplyingl) ||
+          (syear && sportname && smonth && squantity) ||
+          (ryear && rportname && rmonth && rquantity)
+        )
+      ) {
         setError("input minimum required data!");
       }
       if (recievingl) {
         const recievinglist = getArray(recievingl);
         const cred = {
           _id: id,
-          recievinglist
+          recievinglist,
         };
         var hasrecievingl = await Axios.put(
           "http://localhost:5000/nodes/update/recievepoints",
@@ -70,7 +87,7 @@ export default function Updatenode() {
         const distributionlist = getArray(supplyingl);
         const cred = {
           _id: id,
-          distributionlist
+          distributionlist,
         };
         var hassupplyingl = await Axios.put(
           "http://localhost:5000/nodes/update/supplypoints",
@@ -87,7 +104,7 @@ export default function Updatenode() {
           port_name: sportname,
           quantity: squantity,
           month: smonth,
-          year: syear
+          year: syear,
         };
         var hassupplyobj = await Axios.put(
           "http://localhost:5000/nodes/update/supplyobj",
@@ -104,15 +121,15 @@ export default function Updatenode() {
           port_name: rportname,
           quantity: rquantity,
           month: rmonth,
-          year: ryear
+          year: ryear,
         };
         var hasrecieveobj = await Axios.put(
           "http://localhost:5000/nodes/update/recievedobj",
           cred
         );
         console.log(hasrecieveobj);
-        if (!hasrecieveobj) { 
-         setError("not valid recieved data");
+        if (!hasrecieveobj) {
+          setError("not valid recieved data");
         }
       }
 
@@ -129,23 +146,45 @@ export default function Updatenode() {
 
   if (redirect) return <Redirect to="/dashboard" />;
 
-  
   return (
     <>
       <section className="container ">
         <h1 className="center">Update Node</h1>
         {error ? <ErrorHandle data={error} /> : null}
+        {/* {error ? error : null} */}
         <div className="row">
           <div className="col s12 l4">
             <h5>Node Basic Details</h5>
             <form>
               <div className="input-feild">
-                <label htmlFor="customer_id"> Id</label>
-                <input
+                {/* <label htmlFor="customer_id"> Id</label> */}
+                {/* <input
                   id="customer_id"
                   type="text"
-                  onChange={e => setId(e.target.value)}
-                />
+                  onChange={(e) => setId(e.target.value)}
+                /> */}
+                <label>Select a Node</label>
+                <select
+                  class="browser-default"
+                  onChange={(e) => {
+                    console.log(nodes.find((v) => v._id == e.target.value));
+                    setRecievingNodes(
+                      nodes.find((v) => v._id == e.target.value).recieving_list
+                    );
+                    setDistributionList(
+                      nodes.find((v) => v._id == e.target.value)
+                        .distribution_list
+                    );
+                    setId(e.target.value);
+                  }}
+                >
+                  <option value="" disabled selected>
+                    Choose a node
+                  </option>
+                  {nodes.map((v, i) => {
+                    return <option value={v._id}>{v.port_name}</option>;
+                  })}
+                </select>
               </div>
               <h5>Add Recieve/Supply Points</h5>
               <br />
@@ -154,15 +193,16 @@ export default function Updatenode() {
                 <input
                   id="recievingl"
                   type="text"
-                  onChange={e => setRecievingl(e.target.value)}
+                  onChange={(e) => setRecievingl(e.target.value)}
                 />
+                {/* temp1.find(v => v.port_name = "node1") */}
               </div>
               <div className="input-feild">
                 <label htmlFor="supplyingl">Enter Supply Points</label>
                 <input
                   id="supplyingl"
                   type="text"
-                  onChange={e => setSupplyingl(e.target.value)}
+                  onChange={(e) => setSupplyingl(e.target.value)}
                 />
               </div>
             </form>
@@ -172,19 +212,31 @@ export default function Updatenode() {
             <h5>Supplied Water Details</h5>
             <form>
               <div className="input-feild">
-                <label htmlFor="sportname"> Supply To Port Name</label>
+                {/* <label htmlFor="sportname"> Supply To Port Name</label>
                 <input
                   id="sportname"
                   type="text"
-                  onChange={e => setSportname(e.target.value)}
-                />
+                  onChange={(e) => setSportname(e.target.value)}
+                /> */}
+                <label>Select a Distribution list</label>
+                <select
+                  class="browser-default"
+                  onChange={(e) => setRportname(e.target.value)}
+                >
+                  <option value="" disabled selected>
+                    Choose a distribution list
+                  </option>
+                  {distributionList.map((v, i) => {
+                    return <option value={v._id}>{v.port_name}</option>;
+                  })}
+                </select>
               </div>
               <div className="input-feild">
                 <label htmlFor="squantity">Supplied quantity</label>
                 <input
                   id="squantity"
                   type="text"
-                  onChange={e => setSquantity(e.target.value)}
+                  onChange={(e) => setSquantity(e.target.value)}
                 />
               </div>
               <div className="input-feild">
@@ -192,7 +244,7 @@ export default function Updatenode() {
                 <input
                   id="smonth"
                   type="text"
-                  onChange={e => setSmonth(e.target.value)}
+                  onChange={(e) => setSmonth(e.target.value)}
                 />
               </div>
               <div className="input-feild">
@@ -200,7 +252,7 @@ export default function Updatenode() {
                 <input
                   id="syear"
                   type="text"
-                  onChange={e => setSyear(e.target.value)}
+                  onChange={(e) => setSyear(e.target.value)}
                 />
               </div>
             </form>
@@ -210,19 +262,31 @@ export default function Updatenode() {
             <h5>Recieved Water Details</h5>
             <form>
               <div className="input-feild">
-                <label htmlFor="rportname"> Recieved From Port Name</label>
+                {/* <label htmlFor="rportname"> Recieved From Port Name</label>
                 <input
                   id="rportname"
                   type="text"
-                  onChange={e => setRportname(e.target.value)}
-                />
+                  onChange={(e) => setRportname(e.target.value)}
+                /> */}
+                <label>Select a recieving node</label>
+                <select
+                  class="browser-default"
+                  onChange={(e) => setRportname(e.target.value)}
+                >
+                  <option value="" disabled selected>
+                    Choose a recieving node
+                  </option>
+                  {recievingNodes.map((v, i) => {
+                    return <option value={v._id}>{v.port_name}</option>;
+                  })}
+                </select>
               </div>
               <div className="input-feild">
                 <label htmlFor="rquantity">Recieved quantity</label>
                 <input
                   id="rquantity"
                   type="text"
-                  onChange={e => setRquantity(e.target.value)}
+                  onChange={(e) => setRquantity(e.target.value)}
                 />
               </div>
               <div className="input-feild">
@@ -230,7 +294,7 @@ export default function Updatenode() {
                 <input
                   id="rmonth"
                   type="text"
-                  onChange={e => setRmonth(e.target.value)}
+                  onChange={(e) => setRmonth(e.target.value)}
                 />
               </div>
               <div className="input-feild">
@@ -238,25 +302,17 @@ export default function Updatenode() {
                 <input
                   id="ryear"
                   type="text"
-                  onChange={e => setRyear(e.target.value)}
+                  onChange={(e) => setRyear(e.target.value)}
                 />
               </div>
             </form>
           </div>
         </div>
         {
-                disable ? (
-                  <button  className="btn-large disabled" onClick={submit}>
-                Submit
-              </button>
-                ) :
-                 (
-                   <button  className="btn-large" onClick={submit}>
-                Submit
-              </button>
-              
-              )
-              }
+          <button className="btn-large" onClick={submit}>
+            Submit
+          </button>
+        }
       </section>
     </>
   );
